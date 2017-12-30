7304d612f00e2dd7e10c9ae3fc4ea33fb88dc5a0
akihito.nakano <sora.akatsuki@gmail.com>
2017-12-30 11:49:50 +0900

    Extract constant as a path to data which contains server state

--- examples/server.php
+++ examples/server.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/send_request.php';
 
-if (trim(file_get_contents(__DIR__ . '/server.data')) === SERVER_STATE_ABNORMAL) {
+if (trim(file_get_contents(SERVER_STATE_DATA)) === SERVER_STATE_ABNORMAL) {
     sleep(5);
     header('HTTP/1.1 503 Service Unavailable');
     exit;

53f3bb06a90e06a9718435c2c6d71546e8c8e0fb
akihito.nakano <sora.akatsuki@gmail.com>
2018-01-06 21:55:40 +0900

    Add tests

--- tests/Ackintosh/Ganesha/Storage/Adapter/RedisTest.php
+++ tests/Ackintosh/Ganesha/Storage/Adapter/RedisTest.php
@@ -53,6 +53,76 @@ class RedisTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessageRegExp /\AFailed to add sorted set/
+     */
+    public function incrementThrowsExceptionWhenFailedToRunzAdd()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('zAdd')
+            ->willReturn(false);
+
+        (new Redis($mock))->increment($this->resource);
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessage exception test
+     */
+    public function incrementThrowsException()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('zAdd')
+            ->willThrowException(new \RedisException('exception test'));
+
+        (new Redis($mock))->increment($this->resource);
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessageRegExp /\AFailed to remove expired elements/
+     */
+    public function loadThrowsExceptionWhenFailedToRunzRemRangeByScore()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('zRemRangeByScore')
+            ->willReturn(false);
+
+        (new Redis($mock))->load($this->resource);
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessageRegExp /\AFailed to load cardinality/
+     */
+    public function loadThrowsExceptionWhenFailedToRunzCard()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('zCard')
+            ->willReturn(false);
+
+        (new Redis($mock))->load($this->resource);
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessage exception test
+     */
+    public function loadThrowsException()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('zRemRangeByScore')
+            ->willThrowException(new \RedisException('exception test'));
+
+        (new Redis($mock))->load($this->resource);
+    }
+
+    /**
+     * @test
      */
     public function loadLastFailureTime()
     {
@@ -69,6 +139,32 @@ class RedisTest extends \PHPUnit_Framework_TestCase
             null,
             1
         );
+    }
+
+    /**
+     * @test
+     */
+    public function loadLastFailureTimeReturnsNullIfNoData()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('zRange')
+            ->willReturn(false);
+
+        $this->assertNull((new Redis($mock))->loadLastFailureTime($this->resource));
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessage exception test
+     */
+    public function loadLastFailureTimeThrowsException()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('zRange')
+            ->willThrowException(new \RedisException('exception test'));
+
+        (new Redis($mock))->loadLastFailureTime($this->resource);
     }
 
     /**
@@ -92,5 +188,61 @@ class RedisTest extends \PHPUnit_Framework_TestCase
     {
         $this->redisAdapter->saveStatus($this->resource, Ganesha::STATUS_TRIPPED);
         $this->assertSame(Ganesha::STATUS_TRIPPED, $this->redisAdapter->loadStatus($this->resource));
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessageRegExp /\AFailed to save status/
+     */
+    public function saveStatusThrowsExceptionWhenFailedToRunset()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('set')
+            ->willReturn(false);
+
+        $this->assertNull((new Redis($mock))->saveStatus($this->resource, Ganesha::STATUS_TRIPPED));
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessage exception test
+     */
+    public function saveStatusThrowsException()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('set')
+            ->willThrowException(new \RedisException('exception test'));
+
+        (new Redis($mock))->saveStatus($this->resource, Ganesha::STATUS_TRIPPED);
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessageRegExp /\AFailed to load status/
+     */
+    public function loadStatusThrowsExceptionWhenFailedToRunset()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('get')
+            ->willReturn(false);
+
+        $this->assertNull((new Redis($mock))->loadStatus($this->resource));
+    }
+
+    /**
+     * @test
+     * @expectedException \Ackintosh\Ganesha\Exception\StorageException
+     * @expectedExceptionMessage exception test
+     */
+    public function loadStatusThrowsException()
+    {
+        $mock = $this->getMockBuilder(\Redis::class)->getMock();
+        $mock->method('get')
+            ->willThrowException(new \RedisException('exception test'));
+
+        (new Redis($mock))->loadStatus($this->resource);
     }
 }

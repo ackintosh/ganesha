16a7b268dd2703d187e98aa7050c6b89604badee
akihito.nakano <sora.akatsuki@gmail.com>
2018-01-26 21:46:48 +0900

    Setup php-vcr

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,16 @@
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 $vcrConfiguration = \VCR\VCR::configure();
-$vcrConfiguration->setCassettePath(__DIR__ . 'VcrFixtures');
+$vcrConfiguration->setCassettePath(__DIR__ . '/VcrFixtures');
+$vcrConfiguration->enableLibraryHooks([
+    'stream_wrapper',
+    'curl',
+]);
+$vcrConfiguration->enableRequestMatchers([
+    'method',
+    'url',
+    'query_string',
+    'host',
+    'body',
+    'post_fields'
+]);

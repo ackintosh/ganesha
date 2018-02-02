03fdb107ab1039aeb23fc898ba63632b3873a48d
ackintosh <sora.akatsuki@gmail.com>
2018-02-02 13:58:36 +0900

    Rename doc -> docs

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
     "site_title"   => "Ganesha - PHP implementation of Circuit Breaker pattern",
-    "template_dir" => dirname(__FILE__) . "/doc-templates",
-    "source_dir"   => dirname(__FILE__) . "/doc-source",
+    "template_dir" => dirname(__FILE__) . "/docs-templates",
+    "source_dir"   => dirname(__FILE__) . "/docs-source",
 ];

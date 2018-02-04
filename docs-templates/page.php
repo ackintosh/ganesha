f1bd14f8f013600cb06c8ce59b812f4df481057a
akihito.nakano <sora.akatsuki@gmail.com>
2018-02-04 13:52:09 +0900

    HTML5

--- docs-templates/page.php
+++ docs-templates/page.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by TEMPLATED
-http://templated.co
-Released for free under the Creative Commons Attribution License
-
-Name       : Pastime 
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20140107
-
--->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?= $config->site_title() ?></title>

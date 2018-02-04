b2b2e3176b34595145339ba85f4b4db7fffdc28e
akihito.nakano <sora.akatsuki@gmail.com>
2018-02-04 14:30:41 +0900

    Tweak

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  * Configurations for PR site generate by Soushi
  */
 return [
-    "site_title"   => "Ganesha - PHP implementation of Circuit Breaker pattern",
-    "template_dir" => dirname(__FILE__) . "/docs-templates",
-    "source_dir"   => dirname(__FILE__) . "/docs-source",
+    'site_title'   => 'Ganesha - PHP implementation of Circuit Breaker pattern',
+    'template_dir' => dirname(__FILE__) . '/docs-templates',
+    'source_dir'   => dirname(__FILE__) . '/docs-source',
 ];

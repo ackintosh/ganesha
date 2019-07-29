2c6e53a34aff1dfcce603599ef11dca260617088
ackintosh <sora.akatsuki@gmail.com>
2019-07-29 14:04:45 +0900

    Delete debug codes

--- install_memcached.sh
+++ install_memcached.sh
@@ -1,7 +1,4 @@
-php -v
-php -v | cut -d ' ' -f 2 
-php -v | cut -d ' ' -f 2 | head -n 1
-
+# Workaround for PHP5.6
 if [ "$(php -v | cut -d ' ' -f 2 | head -n 1)" == '5.6.40' ]; then
 	printf "\n" | pecl install memcached-2.2.0
 fi

05110ca01fc52066dcfb3005e89311c514be6843
ackintosh <sora.akatsuki@gmail.com>
2019-07-29 09:57:43 +0900

    install the module without the prompts

--- install_memcached.sh
+++ install_memcached.sh
@@ -3,5 +3,5 @@ php -v | cut -d ' ' -f 2
 php -v | cut -d ' ' -f 2 | head -n 1
 
 if [ "$(php -v | cut -d ' ' -f 2 | head -n 1)" == '5.6.40' ]; then
-	pecl install -f memcached-2.2.0
+	printf "\n" | pecl install memcached-2.2.0
 fi

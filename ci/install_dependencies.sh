a867d4be89736ce93837cb846834d8704648319f
akihito.nakano <sora.akatsuki@gmail.com>
2020-01-19 13:25:10 +0900

    Update ext-memcached

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,7 +1,9 @@
+#!/bin/bash -eu
+
 php --version | cut -d ' ' -f 2 | head -n 1 | grep -e '^7\.[12]\.'
 
 if [ $? = 0 ]; then
-  yes '' | pecl install memcached-3.1.0
+  yes '' | pecl install memcached-3.1.5
   yes '' | pecl install redis-5.1.0
   yes '' | pecl install mongodb-1.6.0
 else

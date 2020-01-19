509d9209ccbc39638d3dff754a0f9e548624e0a3
akihito.nakano <sora.akatsuki@gmail.com>
2020-01-19 13:19:05 +0900

    Fix missing dependency

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,6 +1,7 @@
 php --version | cut -d ' ' -f 2 | head -n 1 | grep -e '^7\.[12]\.'
 
 if [ $? = 0 ]; then
+  yes '' | pecl install memcached-3.1.0
   yes '' | pecl install redis-5.1.0
   yes '' | pecl install mongodb-1.6.0
 else

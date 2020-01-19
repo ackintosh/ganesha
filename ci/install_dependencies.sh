6cd84e007595599292df841f48b1565f88a5afb7
akihito.nakano <sora.akatsuki@gmail.com>
2020-01-19 13:42:33 +0900

    Fix installation script stops unintentionally on PHP ~7.3

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,8 +1,11 @@
-#!/bin/bash -eu
+#!/bin/bash
 
 php --version | cut -d ' ' -f 2 | head -n 1 | grep -e '^7\.[12]\.'
+RESULT=$?
 
-if [ $? = 0 ]; then
+set -eu
+
+if [ $RESULT = 0 ]; then
   yes '' | pecl install memcached-3.1.5
   yes '' | pecl install redis-5.1.0
   yes '' | pecl install mongodb-1.6.0

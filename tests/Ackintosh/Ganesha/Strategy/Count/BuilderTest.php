e9d5a459dd274a2cd009d96d0fad08bfb1655cf9
akihito.nakano <sora.akatsuki@gmail.com>
2020-03-01 12:44:12 +0900

    Add tests

---
+++ tests/Ackintosh/Ganesha/Strategy/Count/BuilderTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ackintosh\Ganesha\Strategy\Count;
+
+use Ackintosh\Ganesha\Storage\StorageKeysInterface;
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function storageKeys()
+    {
+        $storageKeys = new class implements StorageKeysInterface {
+            public function prefix(): string { return "test"; }
+            public function success(): string { return "test"; }
+            public function failure(): string { return "test"; }
+            public function rejection(): string { return "test"; }
+            public function lastFailureTime(): string { return "test"; }
+            public function status(): string { return "test"; }
+        };
+        $this->assertInstanceOf('Ackintosh\Ganesha\Strategy\Count\Builder', (new Builder)->storageKeys($storageKeys));
+    }
+}
---
+++ tests/Ackintosh/Ganesha/Strategy/Rate/BuilderTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ackintosh\Ganesha\Strategy\Rate;
+
+use Ackintosh\Ganesha\Storage\StorageKeysInterface;
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function storageKeys()
+    {
+        $storageKeys = new class implements StorageKeysInterface {
+            public function prefix(): string { return "test"; }
+            public function success(): string { return "test"; }
+            public function failure(): string { return "test"; }
+            public function rejection(): string { return "test"; }
+            public function lastFailureTime(): string { return "test"; }
+            public function status(): string { return "test"; }
+        };
+        $this->assertInstanceOf('Ackintosh\Ganesha\Strategy\Rate\Builder', (new Builder)->storageKeys($storageKeys));
+    }
+}
